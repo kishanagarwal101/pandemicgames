c62a4ee3b1eed918a4593513cacfedb627bc1d6c
Arnav Sehgal <arnavsehgal2001@gmail.com>
2021-08-08 23:50:36 +0530

    participant placing done

--- frontend/src/Pages/Wxyz/Wxyz.js
+++ frontend/src/Pages/Wxyz/Wxyz.js
@@ -1,9 +1,11 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect,useRef} from "react";
 import SocketIOClient from "socket.io-client";
 import Chat from "../../Component/Chat/Chat";
 import POST from "../../Requests/POST";
 import styles from "./Wxyz.module.css";
 import { Redirect } from 'react-router-dom';
+import WxyzParticipant from "./WxyzParticipant";
+import WxyzBI from "./Wxyz.jpg";
 
 const Wxyz = (props) => {
   const [socket, setSocket] = useState(null);
@@ -14,6 +16,11 @@ const Wxyz = (props) => {
   const [isAdmin, setIsAdmin] = useState(false);
   const [roomID, setRoomID] = useState(null);
   const [redirect, setRedirect] = useState(false);
+  const [heightCircle,setHeightCircle]=useState('');
+  const [locateX,setLocateX]=useState([]);
+  const [locateY,setLocateY]=useState([]);
+  const circle=useRef(null);
+
 
   useEffect(() => {
     const socket = SocketIOClient("/");
@@ -48,16 +55,46 @@ const Wxyz = (props) => {
 
         socket.on('chatMessage', (payload) => setMessages(prev => [...prev, payload]));
 
-        socket.on('returnToRoomFromWXYZ', ({ admin }) => {
-          if (admin) {
-              setIsAdmin(true);
-          }
-          setRedirect(true)
-        });
+        // socket.on('returnToRoomFromWXYZ', ({ admin }) => {
+        //   if (admin) {
+        //       setIsAdmin(true);
+        //   }
+        //   setRedirect(true)
+        // });
       }
     },[socket,username]);
+  
+  
+  useEffect(() => {
+    setHeightCircle(circle.current.offsetWidth);
+    var i=1;
+    const theta=360/(users.length);
+    const r=circle.current.offsetWidth/2;
+    setLocateX([]);
+    setLocateY([]);
+    while(i<=users.length){
+      const x=r-r*Math.cos((theta*i)* Math.PI / 180)
+      setLocateX(prev=>{
+        return [...prev,x]
+      });
+      const y=r-r*Math.sin((theta*i)* Math.PI / 180);
+      setLocateY(prev=>{
+        return [...prev,y]
+      });
+      console.log(x,y);
+      i++;
+    }
+  }, [users]);
+  
+    
 
+    // const resize=()=>{
+    //   setHeightCircle(circle.current.offsetWidth);
+    //   setLocateX(circle.current.offsetWidth);
+    //   setLocateY(circle.current.offsetWidth/2)
+    // }
 
+    // window.onresize=resize;
 
   if (redirect) {
     return <Redirect
@@ -67,10 +104,32 @@ const Wxyz = (props) => {
         }}
     />
   }
-  
+
+
+  const circleStyles={
+    width:"100%",
+    margin:"22%",
+    height:heightCircle,
+    position:"relative",
+    backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.1), rgba(0, 0, 0, 0.1)), url(${WxyzBI})`,
+    backdropFilter:"saturate(170%)"
+  }
 
   return (
-    <div>
+    <div className={styles.mainwxyz}>
+      <div className={styles.gameArea}>
+      <div className={styles.circle} ref={circle} style={circleStyles}>
+      {users.map((i,index)=>(
+        <WxyzParticipant 
+          i={i}
+          key={index}
+          index={index}
+          locateX={locateX}
+          locateY={locateY}
+        />
+      ))}
+      </div>
+      </div>
       <div className={styles.chatArea}>
         <Chat socket={socket} username={username} messages={messages} />
       </div>
---
+++ frontend/src/Pages/Wxyz/WxyzParticipant.js
@@ -0,0 +1,36 @@
+import React from "react";
+import styles from "./Wxyz.module.css";
+
+
+
+
+function WxyzParticipant(props){
+    console.log(props.i)
+    console.log(props.locateX);
+    console.log("key="+props.index);
+   
+    const lives=[];
+    for(let j=0;j<parseInt(props.i.lives);j++){
+        lives.push("⭐")
+    }
+
+    const location={
+        position:"absolute",
+        left:props.locateX[props.index],
+        top:props.locateY[props.index],
+        transform:"translate(-50%,-50%)"
+    }
+
+ return(
+    <div style={location} className={styles.participantSpot}>
+        <p style={{textAlign:"center"}}>{props.i.username}</p>
+        {lives.map((i)=>(
+            <span>{i}</span>
+        ))
+        }
+    </div>
+ );
+
+}
+
+export default WxyzParticipant;
