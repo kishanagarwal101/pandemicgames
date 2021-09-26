bb1840e929dc5c67401cb4bb2a251a9ac6f7e569
Sanyam <sanyambhaskar5@gmail.com>
2021-09-26 14:54:22 +0530

    fix: username newadmin issue

--- Sockets/GameSockets/Psych/handleDisconnect.js
+++ Sockets/GameSockets/Psych/handleDisconnect.js
@@ -6,25 +6,26 @@ const handleDisconnect = async (io, socket, roomID, username) => {
     if (room) {
         const newUserList = room.users.filter(user => user.username !== username);
         //Check If Admin Left
-        let hasAdmin = false;
+        if(!newUserList[0])return;
+        let hasAdmin = false, newAdmin = newUserList[0].username
         newUserList.forEach(user => {
             if (user.isAdmin) {
                 hasAdmin = true;
+                newAdmin = user.username;
             }
         });
         //Make New Admin
         if (!hasAdmin) {
             newUserList[0].isAdmin = true;
         }
-        io.in(roomID).emit('changeAdmin', { adminUsername: username })
-        await psychModel.findOneAndUpdate({ roomID }, { users: newUserList });
+        io.in(roomID).emit('changeAdmin', { adminUsername: newAdmin })
+        await psychModel.findOneAndUpdate({ roomID }, { users: newUserList, adminUsername: newAdmin });
         io.in(roomID).emit('userLeftPsychRoom', { newGameState: newUserList });
         let unvoted = 0, unguessed = 0;
         newUserList.forEach(user => {
             if (!user.prompt) unguessed++;
             if (!user.hasVoted) unvoted++;
         });
-
         if (unvoted === 0) {
             //SHOW RESULTS
             setTimeout(() => {
